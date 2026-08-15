ebc16642b6bb69da3a5f853fbdc5792ce203bfb3
hrtechgrowths-netizen <hrtechgrowths@gmail.com>
2026-08-15 20:22:22 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,19 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
-
-# For sqlite, we need check_same_thread: False
 if settings.DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
         settings.DATABASE_URL, connect_args={"check_same_thread": False}
     )
 else:
     engine = create_engine(settings.DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 def get_db():
     """
     SQLAlchemy dependency function to yield a database session
@@ -25,3 +20,5 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
